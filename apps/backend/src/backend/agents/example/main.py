f0c5988361f4363da74a0d028b406db8888b7b40
ozuromo <arthurdorigueto@gmail.com>
2025-10-03 03:34:36 -0300

    feat: add history management

--- apps/backend/src/backend/agents/example/main.py
+++ apps/backend/src/backend/agents/example/main.py
@@ -1,6 +1,19 @@
 from langchain.tools import tool
+from langchain_core.messages.utils import trim_messages
 from langgraph.prebuilt import create_react_agent
 from shared.core import get_model, settings
+
+
+def manage_agent_message_history(state: dict) -> dict:
+    messages = trim_messages(
+        state["messages"],
+        strategy="last",
+        token_counter=len,
+        max_tokens=10,
+        start_on="human",
+        end_on=("human", "tool"),
+    )
+    return {"llm_input_messages": messages}
 
 
 @tool
@@ -18,8 +31,9 @@ Todos ficam parados por alguns segundos, até que Joãozinho se levanta.
 model = get_model(settings.DEFAULT_MODEL)
 
 agent = create_react_agent(
-    model=model,
     name="example-react-agent",
+    model=model,
     tools=[get_joke],
     prompt="Responda as perguntas com bom humor e de forma amigável.",
+    pre_model_hook=manage_agent_message_history,
 )
